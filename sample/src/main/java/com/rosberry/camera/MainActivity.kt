28be85728182f4815a20b68fb46b3047e758779c
Vitaly Fedorov <vitaly.fedorov@rosberry.com>
2021-02-18 10:07:53 +0600

    implement camera preview with camerax

--- sample/src/main/java/com/rosberry/camera/MainActivity.kt
+++ sample/src/main/java/com/rosberry/camera/MainActivity.kt
@@ -2,11 +2,18 @@ package com.rosberry.camera
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.Camera
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import com.rosberry.camera.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private val cameraController by lazy { CameraController(this) }
+    private var camera: Camera? = null
+
+    private var torchEnabled = false
 
     private lateinit var binding: ActivityMainBinding
 
@@ -16,7 +23,41 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        lifecycle.addObserver(cameraController)
-        cameraController.setTextureView(binding.textureView)
+        binding.btnTorch.setOnClickListener { toggleTorch() }
+        binding.slider.addOnChangeListener { _, value, _ -> setZoom(value) }
+        startCamera()
     }
+
+    private fun startCamera() {
+        ProcessCameraProvider
+            .getInstance(this)
+            .run {
+                addListener({
+                    val selector = CameraSelector.DEFAULT_BACK_CAMERA
+                    val provider = get()
+                    val preview = Preview.Builder()
+                        .build()
+                        .also { it.setSurfaceProvider(binding.preview.surfaceProvider) }
+
+                    try {
+                        provider.unbindAll()
+                        camera = provider.bindToLifecycle(this@MainActivity, selector, preview)
+                    } catch (e: Exception) {
+                        e.printStackTrace()
+                    }
+                }, ContextCompat.getMainExecutor(this@MainActivity))
+            }
+    }
+
+    private fun setZoom(zoom: Float) {
+        camera?.cameraControl?.setLinearZoom(zoom.coerceIn(0f, 1f))
+    }
+
+    private fun toggleTorch() {
+        torchEnabled = !torchEnabled
+        if (camera?.cameraInfo?.hasFlashUnit() == true) {
+            camera?.cameraControl?.enableTorch(torchEnabled)
+        }
+    }
+
 }
